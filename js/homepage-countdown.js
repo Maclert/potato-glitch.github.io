60169f2d0d57ad3dcf69f9a29a599b69e393b3d3
example <example@example.com>
2020-04-18 21:33:22 +1000

    Revert "Added countdowns to homepage"
    This reverts commit c8c67c7d8d1d117a1f8641c144b812a6c34e0ab4.

--- js/homepage-countdown.js
+++
@@ -1,8 +0,0 @@
-var htmlVar = document.getElementById('countdown');
-var time = div.elements.countdown.value;
-var date = moment.tz(time);
-$('#clock').countdown(date.toDate(), function(event) {
-  $(this).html(event.strftime('%D days %H:%M:%S'));
-});
-var dateString = date.format('dddd, MMMM Do h:mm a');
-document.getElementById("clockString").innerHTML=dateString;
